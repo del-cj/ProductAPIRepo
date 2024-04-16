d61fb9b676b6174f9ecbe367f6c0882fc992f286
del-cj <del.cj126@gmail.com>
2024-04-16 09:44:04 -0400

    lab12 product changes

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Eggplant", Category = "Vegetable", Price = 42 },
+            new Product { Id = 2, Name = "Pantiloons", Category = "Wardrobe", Price = 6M },
+            new Product { Id = 3, Name = "Elysian Spirit Shield", Category = "Off-hand", Price = 100M }
         };
 
         public ProductsController()
